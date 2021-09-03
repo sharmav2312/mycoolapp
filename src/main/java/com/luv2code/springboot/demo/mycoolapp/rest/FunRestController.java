50179127f57bb9b59e3b7337f546ea4e913992b3
sharmav2312 <sharmav2312@gmail.com>
2021-09-03 22:38:14 +0530

    committing actuator

---
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	// expose '/' endpoint to hello world!
+	@GetMapping("/")
+	public String sayHelloWorld() {
+		return "Hello World , Time on Server is: " + LocalDateTime.now();
+	}
+
+}
